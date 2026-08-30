a10d419622a6c573130fd878ed97abadfade1e00
ondisjhb025 <ondisjhb025@student.wethinkcode.co.za>
2026-08-30 22:25:50 +0200

    Added: Documentation for wardservice

--- ingestion-service/src/main/java/co/wethinkcode/healthsafe/controller/WardService.java
+++ ingestion-service/src/main/java/co/wethinkcode/healthsafe/controller/WardService.java
@@ -5,6 +5,9 @@ import co.wethinkcode.healthsafe.service.ReadCsv;
 
 import java.util.List;
 
+/**
+ * Provides the cleaned ward records to the REST application.
+ */
 public class WardService {
 
     private final List<Ward> wards;
